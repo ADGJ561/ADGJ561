aad44b1c3d3d7e6766920c097646799153c1d1e6
airto <airto@LAPTOP-MOCL9CTD>
2021-10-20 14:52:01 +0100

    leng3

---
+++ RedeSocial/src/main/java/com/mycompany/redesocial/Comentario.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.redesocial;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author airto
+ */
+public class Comentario {
+ 
+    private String corpoComentario;
+    private LocalDateTime dataComentario;//é melhor localdatetime
+    
+    public Comentario(String corpoComentario) {
+        this.corpoComentario = corpoComentario;
+        this.dataComentario= LocalDateTime.now();
+    }
+    public void editarComentario(String corpoComentario){
+     
+    }
+}
