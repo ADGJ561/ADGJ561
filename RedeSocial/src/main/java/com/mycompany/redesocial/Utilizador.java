450eae09f787c5a2de1f1454fe68f1a72393558c
RodrigoTigre <1201689@isep.ipp.pt>
2021-10-20 14:52:48 +0100

    boas

---
+++ RedeSocial/src/main/java/com/mycompany/redesocial/Utilizador.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.redesocial;
+
+import java.io.File;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Fac
+ */
+public class Utilizador {
+
+    String filepath = "utilizador.txt";
+
+    private static Scanner scanner;
+
+    public static void main(String[] args) {
+        String login = "";
+        String pwd = "";
+        String filepath = "utilizadores.txt";
+
+    }
+
+    public static void verificarLogin(String login, String pwd, String filepath) {
+        boolean foind = false;
+        String tempUsername = "";
+        String tempPassword = "";
+        try {
+            scanner = new Scanner(new File(filepath));
+            scanner.useDelimiter("[,\n");
+            
+            while  (scanner.hasNext() && !found){
+                tempUsername = scanner.next();
+                tempPassword = scanner.next();
+                if(tempUsername.trim().equals(login.trim())&& tempPassword.trim().equals(pwd.trim()));
+                
+            }
+            
+            
+            
+            
+        } catch (Exception e) {
+
+        }
+    }
+}
