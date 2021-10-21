f08afb9ef9a15023c29e54ba05fe6cd0118c669e
RodrigoTigre <1201689@isep.ipp.pt>
2021-10-21 16:02:24 +0100

    esta melhor

--- RedeSocial/src/main/java/com/mycompany/redesocial/Utilizador.java
+++ RedeSocial/src/main/java/com/mycompany/redesocial/Utilizador.java
@@ -13,20 +13,104 @@ import java.util.Scanner;
  * @author Fac
  */
 public class Utilizador {
-
-    String filepath = "utilizador.txt";
-
+private static int nrUtilizadores;
+    private int codUT;
+    private String nome;
+    //private Data dataNascimento;
+    private String interesses;
+    
+    private String login;
+    private String pwd;
+   
+private static final String NOME_PREDEFINIDO = "Nao definido";
+    private static final String INTERESSES_PREDEFINIDO = "";
+  private static final String LOGIN_PREDEFINIDO = "";
+    private static final String PWD_PREDEFINIDO = "";
+    
     private static Scanner scanner;
 
-    public static void main(String[] args) {
-        String login = "123";
-        String pwd = "123";
-        String filepath = "utilizadores.txt";
-        int id;
-        System.out.println("funciona?");
-       verificarLogin(login,pwd,filepath);
+    public Utilizador() {
+        nrUtilizadores++;
+        this.codUT = nrUtilizadores;
+        this.nome = NOME_PREDEFINIDO;
+//        this.dataNas = new Data();
+        this.interesses = INTERESSES_PREDEFINIDO;
+        this.login = LOGIN_PREDEFINIDO;
+        this.pwd = PWD_PREDEFINIDO;
+        
+    }
+    
+   public Utilizador(String nome, String interesses, String login, String pwd) {
+        nrUtilizadores++;
+        this.codUT = nrUtilizadores;
+        this.nome = nome;
+//        this.dataNas = new Data();
+        this.interesses = interesses;
+        this.login = login;
+        this.pwd = pwd;
+        
+    }
+   
+   
+   public Utilizador(Utilizador u){
+        nrUtilizadores++;
+        this.codUT = nrUtilizadores;
+        this.nome = u.nome;
+//        this.dataNascimento = new Data(u.dataNasc);
+        this.interesses = u.interesses;
+        this.login = u.login;
+        this.pwd = u.pwd;
+   }
+
+    public static int getNrUtilizadores() {
+        return nrUtilizadores;
     }
 
+    public int getCodUT() {
+        return codUT;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getInteresses() {
+        return interesses;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public static void setNrUtilizadores(int nrUtilizadores) {
+        Utilizador.nrUtilizadores = nrUtilizadores;
+    }
+
+    public void setCodUT(int codUT) {
+        this.codUT = codUT;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setInteresses(String interesses) {
+        this.interesses = interesses;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+   
     public static void verificarLogin(String login, String pwd, String filepath) {
         boolean found = false;
         String tempUsername = "";
