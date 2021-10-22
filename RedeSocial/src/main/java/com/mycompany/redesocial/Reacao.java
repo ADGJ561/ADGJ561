946367d0a96e8ad7a05d51635b2976bffbdd088f
airto <airto@LAPTOP-MOCL9CTD>
2021-10-22 10:15:03 +0100

    leng3

--- RedeSocial/src/main/java/com/mycompany/redesocial/Reacao.java
+++ RedeSocial/src/main/java/com/mycompany/redesocial/Reacao.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.redesocial;
 
 /**
@@ -10,5 +6,10 @@ package com.mycompany.redesocial;
  * @author airto
  */
 public class Reacao {
+    boolean tipo;
+
+    public Reacao(boolean tipo) {
+        this.tipo = tipo;
+    }
     
 }
