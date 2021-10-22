7bfb684fabde2a6cde020071a2d579c901a9f1a3
airto <airto@LAPTOP-MOCL9CTD>
2021-10-22 14:14:28 +0100

    leng3

--- RedeSocial/src/main/java/com/mycompany/redesocial/Rede.java
+++ RedeSocial/src/main/java/com/mycompany/redesocial/Rede.java
@@ -5,11 +5,110 @@
  */
 package com.mycompany.redesocial;
 
+//import com.mycompany.utilitarios.Data;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import javax.xml.crypto.Data;
+
 /**
  *
  * @author OLAF
  */
 public class Rede {
-  
+       
+    private final String NOMEREDE = "Social Bit";
+    SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");  
+    private final LocalDate DATACRIACAO = java.time.LocalDate.now();
+    private int qtdUt;
+    
+    private String nomeR;
+    private LocalDate dataC;
+    
+//Arrays
+    private ArrayList<Utilizador> listaUtilizadores = new ArrayList<>();       
+    private ArrayList<PublicacaoPaginas> listaPubPag = new ArrayList<>();       
+
+    
+    public Rede () {
+        this.nomeR = NOMEREDE;
+        this.dataC = DATACRIACAO;
+        this.qtdUt = qtdUt;
+        this.listaUtilizadores = listaUtilizadores;
+        this.listaPubPag = listaPubPag;
+    }
+    
+    public Rede (String nomeR, LocalDate dataC, int qtdUt, ArrayList<Utilizador> listaUtilizadores, ArrayList<PublicacaoPaginas> listaPubPag) {
+        this.nomeR = NOMEREDE;
+        this.dataC = DATACRIACAO;
+        this.qtdUt = qtdUt;
+        this.listaUtilizadores = listaUtilizadores;
+        this.listaPubPag = listaPubPag;
+    }
+
+    public SimpleDateFormat getFormatter() {
+        return formatter;
+    }
+
+    public int getQtdUt() {
+        return qtdUt;
+    }
+
+    public String getNomeR() {
+        return nomeR;
+    }
+
+    public LocalDate getDataC() {
+        return dataC;
+    }
+
+    public ArrayList<Utilizador> getListaUtilizadores() {
+        return listaUtilizadores;
+    }
+
+    public ArrayList<PublicacaoPaginas> getListaPubPag() {
+        return listaPubPag;
+    }
+    
+    private boolean procurarUtilizador (String nome) {
+        for (Utilizador u : listaUtilizadores) 
+            if(u.getNome().compareTo(nome) == 0) {
+                return true;
+            }
+            return false;
+        }   
+    
+        
+    public void registarUtilizador (String nome, Data dataNas, String interesses, String login, String pwd) {
+        boolean x = procurarUtilizador(nome);
+        if (x == false) {
+            Utilizador u = new Utilizador (nome, dataNas, interesses, login, pwd);
+            listaUtilizadores.add(u);
+        }
+    }
+        
+    public void removerUtilizador (String nome, Data dataNas, String interesses, String login, String pwd) {
+        boolean x = procurarUtilizador(nome);
+        if (x == false) {
+            Utilizador u = new Utilizador (nome, dataNas, interesses, login, pwd);
+            listaUtilizadores.remove(u);
+        }
+    }
+    
+    public int contarUtilizadores (ArrayList<Utilizador> listaUtilizadores) {
+        qtdUt = listaUtilizadores.size();
+        return qtdUt;
+    }
+    
+    public void listarUtilizadores (String nome) {
+        int contagem = 0; // Para listar o numero ao utilizador para o selecionar no menu
+        for (Utilizador u: listaUtilizadores)
+            contagem += 1;
+            System.out.println(contagem + ". " + nome);
+        }
+    
+    
+    }
     
 }
